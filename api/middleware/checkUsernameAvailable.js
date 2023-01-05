e082787009efd40911281310cf4366cbe36ccb1d
timothy hoover <bnhilliard>
2023-01-05 14:06:00 -0800

    update

--- api/middleware/checkUsernameAvailable.js
+++ api/middleware/checkUsernameAvailable.js
@@ -1,8 +1,12 @@
 const Auth = require('../auth/auth-model')
 
 module.exports  = async (req, res, next) => {
+
+  let {username, password} = req.body;
+  username = username.replace(/[^a-zA-Z0-9]/g, '').trim();
+  console.log(username)
         try{
-          const [user] = await Auth.findBy({username: req.body.username})
+          const [user] = await Auth.findBy({username})
           if (user) {
             res.status(401).json({message: "username taken"})
           } else {
